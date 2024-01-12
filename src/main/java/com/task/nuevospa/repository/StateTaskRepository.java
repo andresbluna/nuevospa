bba2a666fc1ec21245cfef7b0aa7a0d50ac5c2cf
andresbluna <andres.benjamin.luna@gmail.com>
2024-01-11 23:14:25 -0300

    Add StateTaskrepository interface

---
+++ src/main/java/com/task/nuevospa/repository/StateTaskRepository.java
@@ -0,0 +1,8 @@
+package com.task.nuevospa.repository;
+
+import com.task.nuevospa.model.StateTask;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StateTaskRepository extends JpaRepository<StateTask, Long> {
+
+}
---
+++ src/main/java/com/task/nuevospa/service/StateTaskService.java
@@ -0,0 +1,10 @@
+package com.task.nuevospa.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StateTaskService {
+
+    private final
+}
