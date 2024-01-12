4bd2ac0abbb95512c9d9891c649b0d71c867bd87
andresbluna <andres.benjamin.luna@gmail.com>
2024-01-12 12:15:49 -0300

    Update Usercontroller

--- src/main/java/com/task/nuevospa/controller/TaskController.java
+++ src/main/java/com/task/nuevospa/controller/TaskController.java
@@ -1,4 +1,19 @@
 package com.task.nuevospa.controller;
 
+import com.task.nuevospa.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tasks")
 public class TaskController {
+
+    private final TaskService taskService;
+
+    @Autowired
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
 }
